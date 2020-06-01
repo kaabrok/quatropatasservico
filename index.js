d93b130d123efc577790488f3e9b1d7347f68bac
Alan M Silva <alanfortlink@gmail.com>
2020-06-01 20:27:43 +0100

    update

--- index.js
+++ index.js
@@ -2,9 +2,8 @@ const app = require('./src/app');
 const http = require('http');
 const debug = require('debug')('nodestr:server');
 
+const port = normalizePort(process.env.PORT || '5000');
 
-const port = 5000;
-//const port = normalizePort(process.env.PORT || '7675');
 app.set('port', port);
 
 const server = http.createServer(app);
